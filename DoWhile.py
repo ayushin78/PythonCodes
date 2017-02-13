4612b5209f8d5d6143510375cddacb600b00fedb
ayushin78 <ayushin78@gmail.com>
2017-02-14 00:38:28 +0530

    added secret message code

--- DoWhile.py
+++ DoWhile.py
@@ -12,22 +12,78 @@
 #
 #
 # print("Thank you for entering a number")
-secret_number=7
+# secret_number=7
+# while True:
+#
+#     try:
+#         guess_number = int(input("Guess a number between 1 and 10"))
+#
+#
+#     except ValueError:
+#         print("you did not enter a number")
+#         continue
+#
+#     if(guess_number!=secret_number):
+#         print("tyou guessed a wrong number")
+#
+#     else:
+#         print("you guessed correctly")
+#         break
+
+# from decimal import Decimal as D
+#
+# sum = D(0)
+#
+# sum += D('0.1')
+# sum += D('0.1')
+#
+# sum -= D('0.2')
+#
+# print('sum = ', sum)
+#
+# print('0.1 + 0.1 - 0.2 = ', 0.1 + 0.1 + .1 - .3)
+#
+# print(type(3))
+# print(type(3.14))
+# print(type("3"))
+# print(type('3'))
+# samp_string = "Hello! This is very interesting"
+# print(samp_string[0:4]) #slicing of string
+# print(samp_string[4:])
+#
+# print("Hello " * 5)
+#
+# print(type(str(4)))
+#
+# for char in samp_string:
+#     print(char)
+#
+# for i in range(0, len(samp_string)-1, 2):
+#     print(samp_string[i]+samp_string[i+1])
+
+
 while True:
-
     try:
-        guess_number = int(input("Guess a number between 1 and 10"))
+        msg = input('Enter a string in uppercase : ')
+        secret_msg = ""
+        for i in range(0, len(msg)):
+            secret_msg += str(ord(msg[i]))
 
+        print("Secret message : "+secret_msg)
+
+        original_number=""
+        for i in range(0, len(secret_msg)-1, 2):
+            original_number += chr(int(secret_msg[i] + secret_msg[i+1]))
+
+        print("Original String : " + original_number)
+
+        break
 
     except ValueError:
-        print("you did not enter a number")
-        continue
+        print("please enter a valid string")
 
-    if(guess_number!=secret_number):
-        print("tyou guessed a wrong number")
 
-    else:
-        print("you guessed correctly")
-        break
+
+
 
 
